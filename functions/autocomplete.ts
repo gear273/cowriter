152478fee8fe4f43d44045d4835d939227df2dcc
Szilárd Dóró <doroszilard@gmail.com>
2023-03-20 16:28:14 +0100

    fix preflight checks again

--- functions/autocomplete.ts
+++ functions/autocomplete.ts
@@ -43,7 +43,7 @@ export default async function handler(req: Request, res: Response) {
     process.env.ALLOWED_ORIGIN || "*"
   );
   res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", ["POST", "OPTIONS"]);
+  res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
 
   if (req.method !== "POST" && req.method !== "OPTIONS") {
     return res.status(405).json({ error: "Method Not Allowed" });
